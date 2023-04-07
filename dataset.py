finished binary classifier, fixed scaling and updated model architecture

dataset.py
@@ -1,8 +1,7 @@
 import pandas as pd
+import numpy as np
 import random
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 def _get_data():
     data = pd.read_csv('winequality-white.csv', sep='\t', header=None, index_col=False)
@@ -16,10 +15,12 @@ def _get_data():
         vector = vector.split(';')
         vector = [float(x) for x in vector]
         label = vector[11]
-        if (label <= 5):
-            label = 1
+
+        if label <= 5:
+            label = 0
         else:
-            label = 2
+            label = 1
+            
         vector = vector[0:10]
         parsed_data.append([vector, label])
 
@@ -32,35 +33,20 @@ def _split_data(data):
     test = data[int((len(data)*0.9)):]
 
     train_x = [item[0] for item in train]
-    train_y = [int(item[1]) for item in train]
+    train_y = np.array([int(item[1]) for item in train])
     val_x = [item[0] for item in val]
-    val_y = [int(item[1]) for item in val]
+    val_y = np.array([int(item[1]) for item in val])
     test_x = [item[0] for item in test]
-    test_y = [int(item[1]) for item in test]
-    
-    count = [train_y.count(i) for i in range(1, 3)]
-    print(f'Count of scores before resampling: {count}')
+    test_y = np.array([int(item[1]) for item in test])
 
-    sm = SMOTE(random_state=42)
-    train_x, train_y = sm.fit_resample(train_x, train_y)
-
-    #TODO: tweak the sampling strategies, perhaps with combination of SMOTE and undersampling?
-
-    count = [train_y.count(i) for i in range(1, 3)]
-    print(f'Count of scores after resampling: {count}')
-
-    train_y = [_one_hot_encoding(item) for item in train_y]
-    val_y = [_one_hot_encoding(item) for item in val_y]
+    scaler = StandardScaler()
+    train_x = scaler.fit_transform(train_x)
+    val_x = scaler.transform(val_x)
+    test_x = scaler.transform(test_x)
 
     data = {'train_x': train_x, 'train_y': train_y, 'val_x': val_x, 'val_y': val_y, 'test_x': test_x, 'test_y': test_y}
 
     return data
-
-def _one_hot_encoding(item):
-    encoding = [0] * 2
-    encoding[item-1] = 1
-    return encoding
-
 
 def data_pipeline():
     data = _get_data()

ensemble.py
@@ -25,8 +25,8 @@ class Ensemble():
         self.trained_models = []
         for network in self.neural_networks:
             print(f'--------Training Network: #{self.neural_networks.index(network)+1}')
-            trained_model = network.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[tf.keras.metrics.Accuracy()])
-            trained_model = network.model.fit(x= self.data['train_x'], y=self.data['train_y'], validation_data=(self.data['val_x'], self.data['val_y']), batch_size= 4)
+            trained_model = network.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+            trained_model = network.model.fit(x= self.data['train_x'], y=self.data['train_y'], validation_data=(self.data['val_x'], self.data['val_y']), batch_size=16, epochs=50)
             self.trained_models.append(trained_model)
 
     def make_prediction(self, x, voting_rule):
@@ -37,7 +37,6 @@ class Ensemble():
             x = x.reshape(1,10,1)
             prediction = network.model.predict(x)[0]
             preference = self.prediction_to_profile(prediction)
-            #print(f"preferences for network {self.trained_models.index(network)+1}: {preference}")
             preferences.append(preference)
         return voting_rule(preferences)
 
@@ -54,9 +53,9 @@ class Ensemble():
     
     def prediction_to_profile(self, prediction):
         preferences = []
-        for _ in prediction:
-            best_vote = np.argmax(prediction)
-            preferences.append(best_vote)
-            prediction[best_vote] = -1
+        if prediction >= 0.5:
+            preferences = [1,0]
+        else:
+            preferences = [0,1]
 
-        return [x+1 for x in preferences]
+        return preferences

social_choice.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def plurality(profile):
-    count = np.zeros(11)
+    count = np.zeros(2)
 
     for preference in profile:
         count[preference[0]] += 1